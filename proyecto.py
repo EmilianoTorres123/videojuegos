4b29ffffb4914a5b735510da8d0426e9a76961d4
emiliano <106941242+EmilianoTorres123@users.noreply.github.com>
2023-03-13 18:38:04 -0600

    proyecto actualizado

--- proyecto.py
+++ proyecto.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import re
 import plotly.express as px
 
 st.set_page_config(page_title="videjuegos",
